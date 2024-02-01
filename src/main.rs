3dd1665fc0e207f3514b266ceeb1cde0d9bfe97f
theoboldalex <theoboldalex@gmail.com>
2024-02-01 23:34:50 +0000

    Stubs out fns for default actions

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,35 @@
 use clap::Parser;
-use std::env;
+use std::{env, path::PathBuf};
 
 #[derive(Parser)]
 struct Args {
     cmd: String,
-    bookmark: String,
+    tag: String,
+}
+
+fn list_bookmarks() {
+}
+
+fn add_bookmark(tag: String, dir: PathBuf) {
+    println!("Creating tag: {}, to location: {:?}", tag, dir);
+}
+
+fn remove_bookmark(tag: String) {
+    println!("Removing tag: {}", tag);
+}
+
+fn rename_bookmark(tag: String) {
+    println!("Renaming tag: {}", tag);
 }
 
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
     let cwd = env::current_dir()?;
 
-    println!(
-        "cmd: {:?}, bookmark: {:?}, cwd: {:?}",
-        args.cmd,
-        args.bookmark,
-        cwd.display()
-    );
+    match args.cmd.as_str() {
+        "add" => add_bookmark(args.tag, cwd),
+        _ => println!("Command not yet implemented...")
+    }
 
     Ok(())
 }
